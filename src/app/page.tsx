f3ddb43ec8f5b24ebb6e78426775bfcdf1796648
SCAREcrow0367 <kylermoore65@gmail.com>
2024-08-19 18:39:32 -0600

    texted centered to prep card placement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-auto m-auto">
       <div className="flex justify-center items-center p-auto m-auto">
-        
+        cactus
       </div>
     </main>
   );
